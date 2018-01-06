ab5cbf7676a993c3a815e1eabeb7186a073c5179
moritz157 <moritz.ahrens2@gmail.com>
2018-01-06 02:23:34 +0100

    Fixed build issues and changed appareance

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent{
-  user: Object = {"username": "test1"};
+  user: Object = {"username": "test1", "avatar":"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"};
   navRoutes: Object[] = [{name: "Dashboard", path: "dashboard"}, {name: "Artikel", path: "articles"}, {name: "Termine", path: "events"}, {name: "Mails", path: "mails"}, {name: "Buchhaltung", path: "accounting"}, {name: "Abosystem", path: "subscriptions"}];
   selected="Dashboard";
 
