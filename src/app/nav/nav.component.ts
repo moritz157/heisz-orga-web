a4f8962cccdd3fb6c7662432d1f6dc19cd22f242
moritz157 <moritz.ahrens2@gmail.com>
2018-01-14 00:52:58 +0100

    Fixed avatar generation error and navbar active issue

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, User, Session } from "../services/auth.service";
 
@@ -8,7 +8,7 @@ import { AuthService, User, Session } from "../services/auth.service";
   styleUrls: ['./nav.component.css'],
   providers: [AuthService]
 })
-export class NavComponent{
+export class NavComponent implements OnInit{
   user: Object;// = {"username": "test1", "avatar":"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"};
   navRoutes: Object[] = [{name: "Home", path: "dashboard", mobile: true, icon: "home"}, {name: "Artikel", path: "articles", mobile: true, icon: "create"}, {name: "Termine", path: "events", mobile: true, icon: "event"}, {name: "Mails", path: "mails", mobile: true, icon: "mail"}, {name: "Buchhaltung", path: "accounting", mobile: true, icon: "attach_money"}, {name: "Abosystem", path: "subscriptions", mobile: false, icon: "group"}, {name: "Einstellungen", path: "settings", mobile: true, icon: "settings"}, {name: "Abmelden", path: "logout", mobile: false, icon: "exit_to_app"}];
   selected="Home";
@@ -21,5 +21,16 @@ export class NavComponent{
     console.log("Click:", route['name'], route['path']);
     this.selected=route['name'];
     this.router.navigateByUrl("/"+route['path']);
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      var url = this.router.url;
+      for(var i=0;i<this.navRoutes.length;i++){
+        if("/"+this.navRoutes[i]["path"]==url){
+          this.selected=this.navRoutes[i]["name"];
+        }
+      }
+    })
   }
 }
